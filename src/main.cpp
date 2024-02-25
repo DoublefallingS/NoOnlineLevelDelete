cee4abe4ea74a7f70ef514bc39665439151e2537
Thegoldgames <136522956+Thegoldgames@users.noreply.github.com>
2024-02-24 17:01:20 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,20 @@ class $modify(LevelInfoLayer) {
             DeleteMenu->updateLayout();
         }
 
+        // Right side delete-button (for moderators or FakeModerator mod)
+        auto value = Mod::get()->getSettingValue<bool>("moderator-button-disabled");
+        if (value == true) {
+            auto DeleteMenu2 = this->getChildByID("right-side-menu");
+            if (!DeleteMenu2) {
+                return false;
+            }
+		    auto DeleteButton2 = DeleteMenu2->getChildByID("delete-button");
+            if (!DeleteButton2) {
+            } else {
+                DeleteMenu2->removeChild(DeleteButton2);
+                DeleteMenu2->updateLayout();
+            }
+        }
 		return true;
 	}
 };
